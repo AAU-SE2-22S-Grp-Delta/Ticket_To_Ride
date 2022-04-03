4fb613a5d3043ca32d5aab9588f82a99000fd72d
Christian Schönberg <1726822+chrisch1974@users.noreply.github.com>
2022-04-03 23:14:33 +0200

    #27 Added start game button

--- app/src/main/java/at/aau/se2/tickettoride/MainActivity.java
+++ app/src/main/java/at/aau/se2/tickettoride/MainActivity.java
@@ -2,13 +2,24 @@ package at.aau.se2.tickettoride;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import at.aau.se2.tickettoride.activities.GameActivity;
+import at.aau.se2.tickettoride.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.button.setOnClickListener(v -> {
+            Intent intent = new Intent(this, GameActivity.class);
+            startActivity(intent);
+        });
     }
 }
