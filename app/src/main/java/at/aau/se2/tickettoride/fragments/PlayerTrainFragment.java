4cdd17c9f338ca52370fdfb760f4d530f9b6a8eb
Philipp <philipprauscher26@gmail.com>
2022-04-11 23:50:37 +0200

    Added onViewCreated

--- app/src/main/java/at/aau/se2/tickettoride/fragments/PlayerTrainFragment.java
+++ app/src/main/java/at/aau/se2/tickettoride/fragments/PlayerTrainFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -23,6 +25,29 @@ public class PlayerTrainFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentPlayerTrainBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        LinearLayout layout = new LinearLayout(view.getContext());
+        layout.setOrientation(LinearLayout.HORIZONTAL);
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        layout.setLayoutParams(layoutParams);
+
+        for(int i=1;i<5;i++)
+        {
+            ImageView imageView = new ImageView(view.getContext());
+            imageView.setLayoutParams(new android.view.ViewGroup.LayoutParams(80,60));
+            imageView.setMaxHeight(20);
+            imageView.setMaxWidth(20);
+
+            layout.addView(imageView);
+        }
     }
 
     @Override
