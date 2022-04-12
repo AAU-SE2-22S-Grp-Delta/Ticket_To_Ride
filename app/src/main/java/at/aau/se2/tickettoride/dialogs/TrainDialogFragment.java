4107c6eae7ec8d00add2dcefe17d1931b9335f81
SabrinaMuh <sabrina.muhrer@aon.at>
2022-04-12 12:26:22 +0200

    Implement Colors Enum

--- app/src/main/java/at/aau/se2/tickettoride/dialogs/TrainDialogFragment.java
+++ app/src/main/java/at/aau/se2/tickettoride/dialogs/TrainDialogFragment.java
@@ -16,56 +16,67 @@ import androidx.fragment.app.DialogFragment;
 import java.util.Random;
 
 import at.aau.se2.tickettoride.R;
+import at.aau.se2.tickettoride.enums.Colors;
 
 public class TrainDialogFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        Colors color;
+
+        //Create View
         LinearLayout layout = new LinearLayout(getActivity());
-        Random random = new Random();
-        int randomNumber = random.nextInt(8);
         ImageView imageView = new ImageView(getActivity());
+        //Generate Random color
+        Random random = new Random();
+        int randomNumber = random.nextInt(7);
         switch (randomNumber){
             case 0: //Black
                 imageView.setImageResource(R.drawable.ic_train_black);
+                color = Colors.BLACK;
                 break;
             case 1: //Blue
                 imageView.setImageResource(R.drawable.ic_train_blue);
+                color = Colors.BLUE;
                 break;
             case 2: //Green
                 imageView.setImageResource(R.drawable.ic_train_green);
+                color = Colors.GREEN;
                 break;
             case 3: //Orange
                 imageView.setImageResource(R.drawable.ic_train_orange);
+                color = Colors.ORANGE;
                 break;
             case 4: //Purpur
                 imageView.setImageResource(R.drawable.ic_train_purpur);
+                color = Colors.PURPUR;
                 break;
             case 5: //Red
                 imageView.setImageResource(R.drawable.ic_train_red);
+                color = Colors.RED;
                 break;
             case 6: //White
                 imageView.setImageResource(R.drawable.ic_train_white);
+                color = Colors.WHITE;
                 break;
-            case 7: //Yellow
+            default: //Yellow
                 imageView.setImageResource(R.drawable.ic_train_yellow);
-                break;
-            default:
-                imageView.setImageResource(R.drawable.ic_train_black);
+                color = Colors.YELLOW;
                 break;
         }
-
         imageView.setMinimumHeight(500);
         layout.setOrientation(LinearLayout.VERTICAL);
         layout.addView(imageView);
 
+        //Set Dialog Window
         builder.setView(layout)
                 .setTitle("Deine neue Waagonkarte")
                 .setPositiveButton(R.string.accept, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int id) {
-
+                        //TODO Send color to hand
+                        Log.i("COLOR", color.name());
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
