9a442a575a01d4d9edcc2982b93f2142a54b3528
Wolfgang Amadeus <der.wolfgang.amadeus.mozart@gmx.at>
2022-04-12 19:35:24 +0200

    map dirty impl scrolling and zooming by finger

--- app/src/main/java/at/aau/se2/tickettoride/fragments/MapFragment.java
+++ app/src/main/java/at/aau/se2/tickettoride/fragments/MapFragment.java
@@ -1,13 +1,14 @@
 package at.aau.se2.tickettoride.fragments;
 
 import android.annotation.SuppressLint;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsoluteLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -32,48 +33,15 @@ public class MapFragment extends Fragment {
         binding.mapPanel.setScaleX(1.f);
         binding.mapPanel.setScaleY(1.f);
 
-        binding.mapPanel.setOnTouchListener(new View.OnTouchListener() {
-            private float mx, my, curX, curY;
-            private boolean started = false;
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
+        binding.mapPanel.setOnTouchListener(new MapOnTouchListener());
 
-                curX = motionEvent.getX();
-                curY = motionEvent.getY();
-                int dx = (int) (mx - curX);
-                int dy = (int) (my - curY);
-                Log.d("onTouchListener", "curX:" + curX);
-                Log.d("onTouchListener", "curY:" + curX);
-                Log.d("onTouchListener", "mx:" + mx);
-                Log.d("onTouchListener", "my:" + my);
-
-                switch (motionEvent.getAction()) {
-                    case MotionEvent.ACTION_MOVE:
-                        if (started) {
-                            binding.mapPanel.scrollBy(dx,dy);
-                        } else {
-                            started = true;
-                        }
-                        mx = curX;
-                        my = curY;
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        started = false;
-                        Log.d("onTouchListner=stopp", "scrollX="+binding.mapPanel.getScrollX() + ", scrollY=" + binding.mapPanel.getScrollY());
-                        break;
-                }
-
-
-                return true;
-            }
-        });
 //        view.setOnTouchListener(this);
 
         binding.buttonAbove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                binding.mapPanel.setScaleX(binding.mapPanel.getScaleX()+.2f);
-                binding.mapPanel.setScaleY(binding.mapPanel.getScaleY()+.2f);
+                binding.mapPanel.setScaleX(binding.mapPanel.getScaleX() + .2f);
+                binding.mapPanel.setScaleY(binding.mapPanel.getScaleY() + .2f);
                 Log.d("buttonPos", "x=" + binding.buttonAbove.getX() + ", y=" + binding.buttonAbove.getY());
                 Log.d("mapPanelPos", "x=" + binding.mapPanel.getX() + ", y=" + binding.mapPanel.getY());
                 Log.d("mapPanelSize", "x=" + binding.mapPanel.getWidth() + ", y=" + binding.mapPanel.getHeight());
@@ -81,15 +49,14 @@ public class MapFragment extends Fragment {
         });
 
         binding.buttonDownside.setOnClickListener(view1 -> {
-            binding.mapPanel.setScaleX(binding.mapPanel.getScaleX()-.2f);
-            binding.mapPanel.setScaleY(binding.mapPanel.getScaleY()-.2f);
+            binding.mapPanel.setScaleX(binding.mapPanel.getScaleX() - .2f);
+            binding.mapPanel.setScaleY(binding.mapPanel.getScaleY() - .2f);
             Log.d("buttonPos", "x=" + binding.buttonDownside.getX() + ", y=" + binding.buttonDownside.getY());
             Log.d("buttonSize", "x=" + binding.buttonDownside.getWidth() + ", y=" + binding.buttonDownside.getHeight());
             Log.d("mapPanelPos", "x=" + binding.mapPanel.getX() + ", y=" + binding.mapPanel.getY());
             Log.d("mapPanelSize", "x=" + binding.mapPanel.getWidth() + ", y=" + binding.mapPanel.getHeight());
             View mapPanel = binding.mapPanel;
         });
-
 
         return view;
     }
@@ -101,14 +68,88 @@ public class MapFragment extends Fragment {
         binding = null;
     }
 
-//    @Override
-//    /**
-//     * prints the motion event to a textbox to visualize what is happening
-//     */
-//    public boolean onTouch(View view, MotionEvent motionEvent) {
-//        String message = "";
-//        message += motionEvent.toString() + ", x: " + motionEvent.getX() + ", y: " + motionEvent.getY();
-//        binding.message.setText(message);
-//        return true;
-//    }
+    private class MapOnTouchListener implements View.OnTouchListener {
+        private float mx, my, curX, curY;
+        private float zoomLength0, zoomLength;
+        float scale = 1f;
+        int smoothScale = 0;
+        private boolean scrollingStarted = false, zoomingStarted = false;
+
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+//            Log.d("onTouchListener.Action", "" + motionEvent.getAction());
+            //0=Action_Down, 1=Action_UP, 2=Action_Move, 6=ActionPointer_UP, 261=2ndPointerUp?
+
+
+//            Log.d("onTouchListener", "curX:" + curX);
+//            Log.d("onTouchListener", "curY:" + curX);
+//            Log.d("onTouchListener", "mx:" + mx);
+//            Log.d("onTouchListener", "my:" + my);
+
+            switch (motionEvent.getPointerCount()) {
+
+                case 1: {
+                    curX = motionEvent.getX();
+                    curY = motionEvent.getY();
+                    int dx = (int) (mx - curX);
+                    int dy = (int) (my - curY);
+
+                    switch (motionEvent.getAction()) {
+                        case MotionEvent.ACTION_MOVE:
+                            if (scrollingStarted) {
+                                binding.mapPanel.scrollBy(dx, dy);
+                            } else {
+                                scrollingStarted = true;
+                            }
+                            mx = curX;
+                            my = curY;
+                            break;
+                        case MotionEvent.ACTION_UP:
+                            scrollingStarted = false;
+//                            Log.d("mapPanel.scrollingPos", "scrollX=" + binding.mapPanel.getScrollX() + ", scrollY=" + binding.mapPanel.getScrollY());
+                            break;
+                    }
+
+                }
+                break;
+                case 2: {
+
+
+                    Log.d("onTouchListener.Action", "" + motionEvent.getAction());
+                    float x0 = motionEvent.getX(0);
+                    float y0 = motionEvent.getY(0);
+                    float x1 = motionEvent.getX(1);
+                    float y1 = motionEvent.getY(1);
+
+                    switch (motionEvent.getAction()) {
+                        case MotionEvent.ACTION_MOVE:
+                            if (zoomingStarted) {
+                                zoomLength = (float) Math.sqrt(Math.pow(x0 - x1, 2) + Math.pow(y0 - y1, 2));
+                                Log.d("onTouchListener.zoom", "zoomFactor=" + zoomLength / zoomLength0 + ", l0=" + zoomLength0 + ", l1=" + zoomLength);
+                                smoothScale++;
+//                                if (smoothScale == 1) {
+                                    float tempScale = scale * zoomLength/zoomLength0;
+                                    binding.mapPanel.setScaleX(tempScale);
+                                    binding.mapPanel.setScaleY(tempScale);
+//                                    smoothScale = 0;
+//                                }
+                            } else {
+                                zoomLength0 = (float) Math.sqrt(Math.pow(x0 - x1, 2) + Math.pow(y0 - y1, 2));
+                                zoomingStarted = true;
+                            }
+                            break;
+                        case MotionEvent.ACTION_POINTER_UP:
+                            zoomingStarted = false;
+                            scale *= zoomLength / zoomLength0;
+                            binding.mapPanel.setScaleX(scale);
+                            binding.mapPanel.setScaleY(scale);
+                            Log.d("onTouchListener.zoom", "stop zooming, scale=" + scale);
+                            break;
+                    }
+                }
+                break;
+            }
+            return true;
+        }
+    }
 }
