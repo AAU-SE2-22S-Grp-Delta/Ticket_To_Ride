d41441e6ad7275e2a4401a4636da8870e4305839
Philipp <philipprauscher26@gmail.com>
2022-04-19 18:41:29 +0200

    Updated PlayerDestinationFragment

--- app/src/main/java/at/aau/se2/tickettoride/fragments/PlayerDestinationFragment.java
+++ app/src/main/java/at/aau/se2/tickettoride/fragments/PlayerDestinationFragment.java
@@ -4,15 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
+import at.aau.se2.tickettoride.R;
 import at.aau.se2.tickettoride.databinding.FragmentPlayerDestinationBinding;
 
 public class PlayerDestinationFragment extends Fragment {
     private FragmentPlayerDestinationBinding binding;
+    private LinearLayout linearLayout;
 
     public static PlayerDestinationFragment newInstance() {
         return new PlayerDestinationFragment();
@@ -23,6 +29,36 @@ public class PlayerDestinationFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentPlayerDestinationBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ArrayList<Integer> test = new ArrayList<>();
+        test.add(1);
+        test.add(2);
+        test.add(3);
+
+        linearLayout = (LinearLayout) view.findViewById(R.id.linearLayoutTrackCards);
+
+        for(int i=0;i<=test.size()-1;i++)
+        {
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            layoutParams.setMargins(20,30,20,30);
+
+
+            ImageView imageView = new ImageView(linearLayout.getContext());
+            imageView.setLayoutParams(layoutParams);
+            imageView.getLayoutParams().height = 600;
+            imageView.getLayoutParams().width = 350;
+            imageView.setBackgroundColor(test.get(i));
+
+            linearLayout.addView(imageView);
+        }
     }
 
     @Override
