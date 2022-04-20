20af5d470537d99ce3f142a4661c8b1d146417a2
Christian Schönberg <1726822+chrisch1974@users.noreply.github.com>
2022-04-20 22:44:26 +0200

    #27 Updated GameModel with current status of the game
    When starting a new game then there the cards are drawn and the GameModel is set accordingly
    After start a refresh is send to all listening fragments

--- app/src/main/java/at/aau/se2/tickettoride/activities/GameActivity.java
+++ app/src/main/java/at/aau/se2/tickettoride/activities/GameActivity.java
@@ -3,11 +3,19 @@ package at.aau.se2.tickettoride.activities;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 import at.aau.se2.tickettoride.databinding.ActivityGameBinding;
+import at.aau.se2.tickettoride.models.GameModel;
 
 public class GameActivity extends AppCompatActivity {
+    private GameModel gameModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +23,79 @@ public class GameActivity extends AppCompatActivity {
         ActivityGameBinding binding = ActivityGameBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        startGame();
+    }
+
+    private void startGame() {
+        gameModel = GameModel.getInstance();
+
+        // Generate a new game (will be made on server)
+        generateTestGame();
+
+        // After starting a new game send refresh to all attached fragments
+        Bundle result = new Bundle();
+        getSupportFragmentManager().setFragmentResult("refresh", result);
+    }
+
+    private void generateTestGame() {
+        List<Integer> trainCards = generateTrainCards();
+        List<Integer> playerTrainCards = Arrays.asList(getNumCards(trainCards, 4));
+        Integer[] deskOpenTrainCards = getNumCards(trainCards, 5);
+        List<Integer> destinationCards = generateDestinationCards();
+        List<Integer> playerDestinationCards = Arrays.asList(getNumCards(destinationCards, 3));
+
+        gameModel.setDeskOpenTrainCards(deskOpenTrainCards);
+        gameModel.setDeskClosedTrainCards(trainCards);
+        gameModel.setDeskDestinationCards(destinationCards);
+        gameModel.setPlayerTrainCards(playerTrainCards);
+        gameModel.setPlayerDestinationCards(playerDestinationCards);
+    }
+
+    @NonNull
+    private List<Integer> generateTrainCards() {
+        List<Integer> trainCards = new ArrayList<>();
+        int[] countTrainCards = new int[9];
+        int generatedCards = 0;
+        while (generatedCards < 110) {
+            int card = ThreadLocalRandom.current().nextInt(1, 9 + 1);
+            int count = countTrainCards[card - 1];
+            if (card != 9 && count >= 12 || card == 9 && count >= 14) {
+                continue;
+            }
+
+            countTrainCards[card - 1]++;
+            trainCards.add(card);
+
+            generatedCards++;
+        }
+        return trainCards;
+    }
+
+    @NonNull
+    private List<Integer> generateDestinationCards() {
+        List<Integer> destinationCards = new ArrayList<>();
+        int generatedCards = 0;
+        while (generatedCards < 30) {
+            int card = ThreadLocalRandom.current().nextInt(1, 30 + 1);
+            if (destinationCards.contains(card)) {
+                continue;
+            }
+
+            destinationCards.add(card);
+
+            generatedCards++;
+        }
+        return destinationCards;
+    }
+
+    @NonNull
+    private Integer[] getNumCards(List<Integer> cards, int count) {
+        Integer[] numCards = new Integer[5];
+        for (int i = 0; i < count; i++) {
+            Integer card = cards.remove(0);
+            numCards[i] = card;
+        }
+        return numCards;
     }
 }
--- app/src/main/java/at/aau/se2/tickettoride/models/GameModel.java
+++ app/src/main/java/at/aau/se2/tickettoride/models/GameModel.java
@@ -1,8 +1,105 @@
 package at.aau.se2.tickettoride.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import at.aau.se2.tickettoride.dataStructures.Map;
+
 /**
  * GameModel-class represents an active game and stores the current game situation
  */
 public class GameModel {
+    private static volatile GameModel instance = null;
 
+    private List<Integer> deskClosedTrainCards = new ArrayList<>();
+    private Integer[] deskOpenTrainCards = new Integer[5];
+    private List<Integer> deskDiscardedTrainCards = new ArrayList<>();
+    private List<Integer> deskDestinationCards = new ArrayList<>();
+    private List<Integer> playerTrainCards = new ArrayList<>();
+    private List<Integer> playerDestinationCards = new ArrayList<>();
+    private int playerColoredTrainCards = 45;
+    private Map map = new Map();
+
+    private GameModel() {
+    }
+
+    public static synchronized GameModel getInstance() {
+        if (instance == null) {
+            instance = new GameModel();
+        }
+        return instance;
+    }
+
+    public List<Integer> getDeskClosedTrainCards() {
+        return deskClosedTrainCards;
+    }
+
+    public void setDeskClosedTrainCards(List<Integer> deskClosedTrainCards) {
+        this.deskClosedTrainCards = deskClosedTrainCards;
+    }
+
+    public Integer[] getDeskOpenTrainCards() {
+        return deskOpenTrainCards;
+    }
+
+    public void setDeskOpenTrainCards(Integer[] deskOpenTrainCards) {
+        this.deskOpenTrainCards = deskOpenTrainCards;
+    }
+
+    public List<Integer> getDeskDiscardedTrainCards() {
+        return deskDiscardedTrainCards;
+    }
+
+    public void setDeskDiscardedTrainCards(List<Integer> deskDiscardedTrainCards) {
+        this.deskDiscardedTrainCards = deskDiscardedTrainCards;
+    }
+
+    public List<Integer> getDeskDestinationCards() {
+        return deskDestinationCards;
+    }
+
+    public void setDeskDestinationCards(List<Integer> deskDestinationCards) {
+        this.deskDestinationCards = deskDestinationCards;
+    }
+
+    public List<Integer> getPlayerTrainCards() {
+        return playerTrainCards;
+    }
+
+    public void setPlayerTrainCards(List<Integer> playerTrainCards) {
+        this.playerTrainCards = playerTrainCards;
+    }
+
+    public List<Integer> getPlayerDestinationCards() {
+        return playerDestinationCards;
+    }
+
+    public void setPlayerDestinationCards(List<Integer> playerDestinationCards) {
+        this.playerDestinationCards = playerDestinationCards;
+    }
+
+    public int getPlayerColoredTrainCards() {
+        return playerColoredTrainCards;
+    }
+
+    public void setPlayerColoredTrainCards(int playerColoredTrainCards) {
+        this.playerColoredTrainCards = playerColoredTrainCards;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+
+    // Special methods
+    public Integer getNextClosedTrainCard() {
+        return deskClosedTrainCards.remove(0);
+    }
+
+    public void addDiscardedTrainCards(Integer trainCard) {
+        deskDiscardedTrainCards.add(trainCard);
+    }
 }
