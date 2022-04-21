0e3e46bb5d366b2536e91a458abb40fe8ab743a7
Christian Schönberg <1726822+chrisch1974@users.noreply.github.com>
2022-04-21 17:54:16 +0200

    #7 First draft of the open draw pile of train cards

--- app/src/main/java/at/aau/se2/tickettoride/fragments/DeskOpenTrainFragment.java
+++ app/src/main/java/at/aau/se2/tickettoride/fragments/DeskOpenTrainFragment.java
@@ -10,9 +10,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import at.aau.se2.tickettoride.databinding.FragmentDeskOpenTrainBinding;
+import at.aau.se2.tickettoride.enums.TrainCards;
+import at.aau.se2.tickettoride.models.GameModel;
 
 public class DeskOpenTrainFragment extends Fragment {
     private FragmentDeskOpenTrainBinding binding;
+    private GameModel gameModel;
 
     public static DeskOpenTrainFragment newInstance() {
         return new DeskOpenTrainFragment();
@@ -22,12 +25,46 @@ public class DeskOpenTrainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentDeskOpenTrainBinding.inflate(inflater, container, false);
+        gameModel = GameModel.getInstance();
         return binding.getRoot();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        displayData();
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void displayData() {
+        Integer[] cards = gameModel.getDeskOpenTrainCards();
+        for (int i = 0; i < cards.length; i++) {
+            Integer card = cards[i];
+            if (card != null) {
+                switch (i) {
+                    case 0:
+                        binding.card1.setText(TrainCards.valueOf(card).toString());
+                        break;
+                    case 1:
+                        binding.card2.setText(TrainCards.valueOf(card).toString());
+                        break;
+                    case 2:
+                        binding.card3.setText(TrainCards.valueOf(card).toString());
+                        break;
+                    case 3:
+                        binding.card4.setText(TrainCards.valueOf(card).toString());
+                        break;
+                    case 4:
+                        binding.card5.setText(TrainCards.valueOf(card).toString());
+                        break;
+                }
+            }
+        }
     }
 }
