434ae127755454586be264ea01a2b52cadb5848c
SabrinaMuh <sabrina.muhrer@aon.at>
2022-04-22 15:01:28 +0200

    Changed Way To Generate Image For Train Card

--- app/src/main/java/at/aau/se2/tickettoride/dialogs/TrainDialogFragment.java
+++ app/src/main/java/at/aau/se2/tickettoride/dialogs/TrainDialogFragment.java
@@ -5,8 +5,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -14,12 +12,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import at.aau.se2.tickettoride.R;
 import at.aau.se2.tickettoride.enums.Colors;
+import at.aau.se2.tickettoride.models.GameModel;
+import at.aau.se2.tickettoride.ResourceHelper;
 
 public class TrainDialogFragment extends DialogFragment {
+
+    GameModel gameModel = GameModel.getInstance();
+    int cardNr;
 
     //TODO Delete Listener
     public interface TrainDialogListener{
@@ -41,50 +45,20 @@ public class TrainDialogFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        Colors color;
+        //Colors color;
 
         //Create View
         LinearLayout layout = new LinearLayout(getActivity());
         ImageView imageView = new ImageView(getActivity());
         //Generate Random color
-        //TODO Change to match Game Structure
-        Random random = new Random();
-        int randomNumber = random.nextInt(7);
-        switch (randomNumber){
-            case 0: //Black
-                imageView.setImageResource(R.drawable.ic_train_black);
-                color = Colors.BLACK;
-                break;
-            case 1: //Blue
-                imageView.setImageResource(R.drawable.ic_train_blue);
-                color = Colors.BLUE;
-                break;
-            case 2: //Green
-                imageView.setImageResource(R.drawable.ic_train_green);
-                color = Colors.GREEN;
-                break;
-            case 3: //Orange
-                imageView.setImageResource(R.drawable.ic_train_orange);
-                color = Colors.ORANGE;
-                break;
-            case 4: //Purpur
-                imageView.setImageResource(R.drawable.ic_train_purpur);
-                color = Colors.PURPUR;
-                break;
-            case 5: //Red
-                imageView.setImageResource(R.drawable.ic_train_red);
-                color = Colors.RED;
-                break;
-            case 6: //White
-                imageView.setImageResource(R.drawable.ic_train_white);
-                color = Colors.WHITE;
-                break;
-            default: //Yellow
-                imageView.setImageResource(R.drawable.ic_train_yellow);
-                color = Colors.YELLOW;
-                break;
-        }
+        //TODO Change to match Game Structure (with ResourceHelper)
+        //TODO Delete it after it is in game.
+        gameModel.setDeskClosedTrainCards(new ArrayList<Integer>(Arrays.asList(3,5,6)));
+        cardNr = gameModel.getNextClosedTrainCard();
+
+        imageView.setImageResource(ResourceHelper.getTrainResource(cardNr));
         imageView.setMinimumHeight(500);
         layout.setOrientation(LinearLayout.VERTICAL);
         layout.addView(imageView);
@@ -96,7 +70,7 @@ public class TrainDialogFragment extends DialogFragment {
                     //TODO Delete Listener
                     @Override
                     public void onClick(DialogInterface dialogInterface, int id) {
-                        listener.onDialogPositiveClick(color);
+                        //listener.onDialogPositiveClick(color);
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
