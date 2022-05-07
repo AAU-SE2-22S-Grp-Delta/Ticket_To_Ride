e935f661ab0cae6126ba95b0161b816811b9540b
SabrinaMuh <sabrina.muhrer@aon.at>
2022-05-07 12:26:49 +0200

    #23  Implement Function for the Sum

--- app/src/main/java/at/aau/se2/tickettoride/helpers/PointsHelper.java
+++ app/src/main/java/at/aau/se2/tickettoride/helpers/PointsHelper.java
@@ -1,5 +1,7 @@
 package at.aau.se2.tickettoride.helpers;
 
+import java.util.ArrayList;
+
 import at.aau.se2.tickettoride.models.GameModel;
 
 public class PointsHelper {
@@ -22,5 +24,32 @@ public class PointsHelper {
             default:
                 throw new IllegalStateException("Unexpected value: " + lengthOfRoute);
         }
+    }
+
+    //TODO Fragen, ob es irgendwo Punkte gibt Weg finden bezüglich Punkte von Ziele
+    public int getPointsForDestinations(int Destination){
+        return 0;
+    }
+
+    //TODO Fragen bezüglich Zwischenergebnis
+    public int calculateSum(){
+        GameModel gameModel = GameModel.getInstance();
+        ArrayList<Integer> playerPlayedDestinations = new ArrayList<>(gameModel.getPlayerPlayedDestinations());
+        ArrayList<Integer> playerDestinationCards = new ArrayList<>(gameModel.getPlayerDestinationCards());
+        //TODO Zwischenergebnis zu Summe rechnen
+        int sum = 0;
+
+        //Punkte von Zielkarten dazuzählen und abziehen
+        for (int i = 0; i < playerDestinationCards.size(); i++) {
+            if (playerDestinationCards.contains(playerDestinationCards.get(i))) sum+=getPointsForDestinations(playerDestinationCards.get(i));
+            else sum-=getPointsForDestinations(playerDestinationCards.get(i));
+        }
+
+        //Zusatzpunkte für längste Strecke
+        //TODO Checken ob Spieler die längste Strecke hat.
+        // Falls Spieler nicht hat, checken ob es überhaupt einen gibt mit längster Stecke.
+        // Falls Gleichstand bekommen die jenigen Spieler 10 Punkte
+
+        return sum;
     }
 }
