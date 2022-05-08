682030d0ac28e99e82fe36534f2b9d2b1580b64f
SabrinaMuh <sabrina.muhrer@aon.at>
2022-05-08 14:11:11 +0200

    Implement Test for GetChoices

---
+++ app/src/test/java/at/aau/se2/tickettoride/DrawDestinationCards.java
@@ -0,0 +1,36 @@
+package at.aau.se2.tickettoride;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import at.aau.se2.tickettoride.dialogs.DestinationDialogFragment;
+import at.aau.se2.tickettoride.models.GameModel;
+
+@RunWith(JUnit4.class)
+public class DrawDestinationCards {
+    DestinationDialogFragment destinationDialogFragment;
+
+    @Before
+    public void SetUp(){
+        ArrayList<Integer> deskDestinationCards = new ArrayList<>(Arrays.asList(1, 2, 3));
+        GameModel gameModel = GameModel.getInstance();
+        gameModel.setDeskDestinationCards(deskDestinationCards);
+        destinationDialogFragment = new DestinationDialogFragment();
+    }
+
+    @Test
+    public void TestGetChoices(){
+        String[] destinations = destinationDialogFragment.getChoices();
+        assertEquals(destinations.length, 3);
+    }
+}
