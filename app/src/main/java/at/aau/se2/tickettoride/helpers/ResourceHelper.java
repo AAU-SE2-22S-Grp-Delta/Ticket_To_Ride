2f07b2d033df9f8af7b003eaa6563d625f31fbd9
Christian Schönberg <1726822+chrisch1974@users.noreply.github.com>
2022-05-22 20:47:02 +0200

    #44 Removed unused getMissionResource() method

--- app/src/main/java/at/aau/se2/tickettoride/helpers/ResourceHelper.java
+++ app/src/main/java/at/aau/se2/tickettoride/helpers/ResourceHelper.java
@@ -1,16 +1,8 @@
 package at.aau.se2.tickettoride.helpers;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
-
-import androidx.core.content.ContextCompat;
 
 import at.aau.se2.tickettoride.R;
 import at.aau.se2.tickettoride.dataStructures.Mission;
@@ -19,26 +11,6 @@ import at.aau.se2.tickettoride.enums.TrainCards;
 import at.aau.se2.tickettoride.models.Missions;
 
 public class ResourceHelper {
-    public static Drawable getMissionResource(Context context, int card) {
-        switch (card) {
-            case 1:
-                return ContextCompat.getDrawable(context, R.drawable.calgary_saltlakecity);
-            case 2:
-                return ContextCompat.getDrawable(context, R.drawable.kansascity_houston);
-            case 3:
-                return ContextCompat.getDrawable(context, R.drawable.losangeles_chicago);
-            case 4:
-                return ContextCompat.getDrawable(context, R.drawable.newyork_atlanta);
-            case 5:
-                return ContextCompat.getDrawable(context, R.drawable.portland_phoenix);
-            case 6:
-                return ContextCompat.getDrawable(context, R.drawable.seattle_newyork);
-            default:
-                Bitmap bitmap = textAsBitmap("Mission " + card, 48, Color.RED);
-                return new BitmapDrawable(context.getResources(), bitmap);
-        }
-    }
-
     public static View getMissionView(Context context, int card) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         CardMissionBinding binding = CardMissionBinding.inflate(inflater);
@@ -73,19 +45,5 @@ public class ResourceHelper {
             default:
                 throw new IllegalStateException("Unexpected value: " + card);
         }
-    }
-
-    private static Bitmap textAsBitmap(String text, float textSize, int textColor) {
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setTextSize(textSize);
-        paint.setColor(textColor);
-        paint.setTextAlign(Paint.Align.LEFT);
-        float baseline = -paint.ascent();
-        int width = (int) (paint.measureText(text) + 0.5f);
-        int height = (int) (baseline + paint.descent() + 0.5f);
-        Bitmap image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(image);
-        canvas.drawText(text, 0, baseline, paint);
-        return image;
     }
 }
