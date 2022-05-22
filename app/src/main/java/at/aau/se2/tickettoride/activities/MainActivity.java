1651d2072488fd2b9deec8245389a39994c63e5e
Christian Schönberg <1726822+chrisch1974@users.noreply.github.com>
2022-05-22 20:00:10 +0200

    #27 Remove unused activities from game start

--- app/src/main/java/at/aau/se2/tickettoride/activities/MainActivity.java
+++ app/src/main/java/at/aau/se2/tickettoride/activities/MainActivity.java
@@ -13,10 +13,8 @@ import at.aau.se2.tickettoride.clientConnection.ClientConnection;
 import at.aau.se2.tickettoride.databinding.ActivityMainBinding;
 import at.aau.se2.tickettoride.dialogs.HelpDialogFragment;
 
-
-
 public class MainActivity extends AppCompatActivity {
-    ClientConnection cc;
+    private ClientConnection cc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         TextInputEditText textInput = new TextInputEditText(builder.getContext());
         builder.setView(textInput);
         builder.setPositiveButton("set", (dialogInterface, i) -> {
-           cc.setIPv4(textInput.getText().toString());
+            cc.setIPv4(textInput.getText().toString());
         });
         builder.create().show();
 
@@ -42,40 +40,6 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-        binding.button1.setOnClickListener(v -> {
-            Intent intent = new Intent(this, MapActivity.class);
-            startActivity(intent);
-        });
-
-        binding.button2.setOnClickListener(v -> {
-            Intent intent = new Intent(this, PlayerDestinationActivity.class);
-            startActivity(intent);
-        });
-
-        binding.button3.setOnClickListener(v -> {
-            Intent intent = new Intent(this, PlayerTrainActivity.class);
-            startActivity(intent);
-        });
-
-        binding.button4.setOnClickListener(v -> {
-            Intent intent = new Intent(this, DeskDestinationActivity.class);
-            startActivity(intent);
-        });
-
-        binding.button5.setOnClickListener(v -> {
-            Intent intent = new Intent(this, DeskTrainActivity.class);
-            startActivity(intent);
-        });
-
-        binding.button6.setOnClickListener(v -> {
-            Intent intent = new Intent(this, DeskOpenTrainActivity.class);
-            startActivity(intent);
-        });
-
-        binding.button7.setOnClickListener(v -> {
-            Intent intent = new Intent(this, DrawDestinationCardsActivity.class);
-            startActivity(intent);
-        });
         binding.floatingActionButton.setOnClickListener(v -> {
             HelpDialogFragment helpDialogFragment = new HelpDialogFragment();
             helpDialogFragment.show(getSupportFragmentManager(), "helpDialog");
