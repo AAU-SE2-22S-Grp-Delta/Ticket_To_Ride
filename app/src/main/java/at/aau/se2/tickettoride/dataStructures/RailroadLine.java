41498fea4bc51af1b9031f6ebec5104ef4fabcfd
Samuel Radler <samradler@edu.aau.at>
2022-05-29 12:14:49 +0200

    Overriding HashCode in RailroadLine.java #100

--- app/src/main/java/at/aau/se2/tickettoride/dataStructures/RailroadLine.java
+++ app/src/main/java/at/aau/se2/tickettoride/dataStructures/RailroadLine.java
@@ -88,7 +88,11 @@ public class RailroadLine
         this.owner = owner;
     }
 
-
+    @Override
+    public int hashCode()
+    {
+        return super.hashCode();
+    }
 
     @Override
     public boolean equals(Object obj)
