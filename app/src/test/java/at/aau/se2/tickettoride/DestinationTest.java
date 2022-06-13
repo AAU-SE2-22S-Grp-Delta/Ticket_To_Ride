5ba4ae2d29d86e69c07e3ada470fa4e049fb4d64
Christian Schönberg <1726822+chrisch1974@users.noreply.github.com>
2022-06-13 21:09:08 +0200

    #26 Adapted Destination test

--- app/src/test/java/at/aau/se2/tickettoride/DestinationTest.java
+++ app/src/test/java/at/aau/se2/tickettoride/DestinationTest.java
@@ -44,7 +44,7 @@ class DestinationTest
     @Test
     void testSetNameUsed()
     {
-        assertThrows(IllegalArgumentException.class, () -> dest1.setName("testdest2"));
+        assertThrows(IllegalArgumentException.class, () -> dest1.setName(null));
     }
 
     @Test
