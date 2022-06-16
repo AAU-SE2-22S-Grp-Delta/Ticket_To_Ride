a71326873c4d611d2742c680f3d8e75f9df7a893
SabrinaMuh <sabrina.muhrer@aon.at>
2022-06-16 15:17:54 +0200

    Add endscreen design

---
+++ app/src/main/java/at/aau/se2/tickettoride/activities/ScoreAdapter.java
@@ -0,0 +1,38 @@
+package at.aau.se2.tickettoride.activities;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import at.aau.se2.tickettoride.R;
+import at.aau.se2.tickettoride.dataStructures.Player;
+
+public class ScoreAdapter extends ArrayAdapter<Player> {
+    ArrayList<Player> players;
+    Activity context;
+
+    public ScoreAdapter(Activity context, ArrayList<Player> players){
+        super(context, R.layout.list_score, players);
+        this.players = players;
+        this.context = context;
+    }
+
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.list_score, null, true);
+
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.imageViewWinner);
+        TextView textView = (TextView) rowView.findViewById(R.id.textViewNamePoints);
+
+        //if (i > 0) imageView.setVisibility(View.INVISIBLE);
+        Player player = players.get(i);
+        textView.setText(player.getName()+": "+player.getPoints());
+        return rowView;
+    }
+}
